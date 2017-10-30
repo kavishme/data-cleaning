850064b03735a44553f5d22e702a42f4470d602f
kavish jain <kavishme@gmail.com>
2017-10-30 13:59:17 -0700

    wip word vector

--- categorize.py
+++ categorize.py
@@ -45,6 +45,7 @@ def getPostsByTags():
 
             print(result[0])
             result = cur.fetchone()
+        
         return postsByTags
     except Exception as err:
         print("ERR: ")
@@ -54,21 +55,30 @@ def getPostsByTags():
 """
 Store posts to CSV files by tags
 """
-def toCSV(postBytags):
+def toCSV(postBytags, singleFile=False, filename="all_data.csv"):
     try:
         outdir = "output_" + datetime.datetime.now().strftime('%Y%d%m%H%M')
         os.makedirs(outdir)
-        for tag in postBytags:
-            with open(os.path.join(outdir, tag+".csv"), 'w') as of:
+        header=["qtags", "qtitle", "qbody","abody","label"]
+        if not singleFile:
+            for tag in postBytags:
+                with open(os.path.join(outdir, tag+".csv"), 'w') as of:
+                    wr = csv.writer(of, quoting=csv.QUOTE_ALL)
+                    wr.writerow(header)
+                    wr.writerows(postBytags[tag])
+        else:
+            with open(os.path.join(outdir, filename), 'w') as of:
                 wr = csv.writer(of, quoting=csv.QUOTE_ALL)
-                wr.writerows(postBytags[tag])
+                wr.writerow(header)
+                for tag in postBytags:
+                    wr.writerows(postBytags[tag])
         return outdir
     except Exception as err:
         print("ERR: ")
         print(err)
 
 
-def random_answer(posts):
+def randomizeAnswerAndLabel(posts):
     cats = list(posts.keys())
     outposts = {}
     for c in cats:
@@ -98,9 +108,56 @@ def cat_count(posts, dir):
     wr.writerows(t)
     f.close()
 
+
+def filterbySize(posts, minl, maxl, filterIndex):
+    cats = list(posts.keys())
+    outposts = {}
+    for c in cats:
+        outposts[c] = []
+        for rec in posts[c]:
+            data = rec[filterIndex]
+            data = data.split()
+            if len(data)>= minl and len(data)<=maxl:
+                outposts[c].append(rec)
+    return outposts
+
+def wordToVec(posts):
+    cats = list(posts.keys())
+    bagOfWords = {}
+    for c in cats:
+        for rec in posts[c]:
+            # 0:"qtags", 1:"qtitle", 2:"qbody", 3:"abody", 4:"label"
+            data = rec[1] + rec[2] + rec[3]
+            for word in data.split():
+                bagOfWords[word] = 0
+    
+    i = 0
+    for item in bagOfWords:
+        bagOfWords[item] = i
+        i += 1
+    
+    outposts = {}
+    for c in cats:
+        outposts[c] = []
+        for rec in posts[c]:
+            data = rec[filterIndex]
+            data = data.split()
+            if len(data)>= minl and len(data)<=maxl:
+                outposts[c].append(rec)
+    return outposts
+
+    
+    return bagOfWords
+
+
 if __name__ == "__main__":
     p = getPostsByTags()
-    #p=random_answer(p)
-    dir = toCSV(p)
-    cat_count(p, dir)
+    
+    # adds label 0 and 1
+    p = randomizeAnswerAndLabel(p)
+
+    # filterIndex 0:"qtags", 1:"qtitle", 2:"qbody", 3:"abody", 4:"label"
+    p = filterbySize(p, 15, 80, 3)
+    outdir = toCSV(p, True)
+    cat_count(p, outdir)
 
